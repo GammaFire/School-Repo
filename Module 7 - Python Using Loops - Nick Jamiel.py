d6de9fad283baaedb038f22cfbf77f7586360b8b
GammaFire <169076091+GammaFire@users.noreply.github.com>
2025-12-14 18:06:04 -0500

    Add files via upload

---
+++ Module 7 - Python Using Loops - Nick Jamiel.py
@@ -0,0 +1,25 @@
+Python 3.14.0 (tags/v3.14.0:ebf955d, Oct  7 2025, 10:15:03) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+>>> for number in numbers:
+...     if(number % 2 == 0):
+...         print(number, "is even")
+...     else:
+...         print(number, "is odd")
+... 
+...         
+1 is odd
+2 is even
+3 is odd
+4 is even
+5 is odd
+6 is even
+7 is odd
+8 is even
+9 is odd
+10 is even
+11 is odd
+12 is even
+13 is odd
+14 is even
+15 is odd
