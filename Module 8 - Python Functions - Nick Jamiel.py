62e3e98eb8fa8139c1e34217e08e473b7d27124a
GammaFire <169076091+GammaFire@users.noreply.github.com>
2025-12-18 19:36:46 -0500

    Add files via upload

---
+++ Module 8 - Python Functions - Nick Jamiel.py
@@ -0,0 +1,19 @@
+def greater_than(a,b):
+    if (a>b): 
+        print("True")
+    else:
+        print("False")
+a=2
+b=3
+
+greater_than(a,b)
+
+def greater_than2(a,b):
+    if (a>b):
+        print("True")
+    else:
+        print("False")
+a=10
+b=6
+
+greater_than2(a,b)
